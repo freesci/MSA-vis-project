55cd93fc6929d96ea63d75b5c401fa6791c62058
e1809 <marta@marta-P5K.(none)>
2011-06-24 12:05:34 +0200

    consensus 2.0

--- consensus.py
+++ consensus.py
@@ -19,25 +19,45 @@ def consensus(seqDict):
 	h=0		#uncertainty
 	r=0		#information content
 	height={}
+	n=0		#number of non-gap letters at position i
+	consensus=[]
 
 	for i in xrange(len(seqDict[id[0]])):
 		for key in id:
-			if aaDict.has_key(seqDict[key][i]):
-				aaDict[seqDict[key][i]]+=1.0
-			else:
-				aaDict[seqDict[key][i]]=1.0
+			if seqDict[key][i]!="-":
+				if aaDict.has_key(seqDict[key][i]):
+					aaDict[seqDict[key][i]]+=1.0
+				else:
+					aaDict[seqDict[key][i]]=1.0
+				n+=1
 
 		for k in sorted(aaDict):
-			aaDict[k]/=len(seqDict)
-			h+=aaDict[k]*log(aaDict[k], 2) # + zamiast - -, by nie robić niepotrzebnych błędów numeryccznych
+			aaDict[k]/=n
+			h+=aaDict[k]*log(aaDict[k], 2)
 
 		r=log(20,2)+h
 		h=0
+		n=0
 
 		for k in sorted(aaDict):
 			height[k]=aaDict[k]*r
-			print "position", i, "height", k, height[k]
+
+
+		first=("", 0)
+		second=("", 0)
+		for k in sorted(height):
+			if height[k] > first[1]:
+				first, second=(k, height[k]), first
+			elif height[k] > second[1]:
+				second=(k, height[k])
+
+		if first[1]>log(20,2)*0.5:
+			consensus.append(first[0])
+		elif (first[1]+second[1])>log(20,2)*0.75:
+			consensus.append(first[0]+'/'+second[0])
+		else:
+			consensus.append('*')
 
 		aaDict={}
-
-#TODO: krytetia wyboru znakow do konsensusu
+		height={}
+	return consensus
