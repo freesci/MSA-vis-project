14f7b96dc9cd1103ccf9c031af31c0e64fd23347
e1809 <marta@marta-P5K.(none)>
2011-07-09 01:02:16 +0200

    chart 2.0

--- chart.py
+++ chart.py
@@ -55,84 +55,84 @@ arguments:
 	hydro - average hydrophobicity (list) 
 	chain - average side chain volume (list)
 	stru - secondary structure (list)
+	col - number of columns (default=30)
 """
 
-def chart(consensus, hydro, chain, stru):
-	col=30	#aas in row
+def chart(consensus, hydro, chain, stru, col=50):
 	minC=60.0	#min side-chain volume
 	maxC=230.0	#max side-chain volume
 	l = len(consensus)
-	rows = l/col
+	if col==0: col=l
+	rows = int(np.ceil(float(l)/col))+1	#number of rows
+	margin = 0.1
+	height=(1-margin*(rows+1))/rows		#barchart height
 
-	fig = plt.figure(figsize =(col/3,(rows+1)*8))
-	plt.subplots_adjust(hspace=0.7)
+	fig = plt.figure(figsize =(col/3,rows*8))
 
-
-	wykr=fig.add_subplot(rows+2, 1, 1, axisbg=None)
+	wykr=plt.axes([margin, 1-(margin+height), 0.8, height])
 	wykr.set_title("MSA Visualisation", y=1)
+	plt.axis('off')
 
 	#side chain volume colorbar
 	m=cm.ScalarMappable(cmap=cm.autumn)
 	m.set_array(np.array([minC, maxC]))
 	cbr=plt.colorbar(m, orientation='horizontal', fraction=0.7)
 	cbr.set_label('Side Chain Volume')
-	plt.axis('off')	
+
 
 	width = 1
 	widths=[1.0/col]*col
-	for r in xrange(rows):
+	for r in xrange(rows-2):
 
 		#barchart
-		tmp=fig.add_subplot(rows+2, 1, r+2, xlabel="Amino Acid", ylabel='Hydrophobicity')
+		tmp=plt.axes([margin, 1-(r+2)*(margin+height), 1-2*margin, height], xlabel="Amino Acid", ylabel='Hydrophobicity')
 		for i in xrange(col):
-			#print "r, i", r, i
 			c = (1, (chain[col*r+i]-minC)/(maxC-minC), 0)    #bar color
 			tmp.bar(col*r+i, hydro[col*r+i], width, color=c, align='center', linewidth=1)
 	    	plt.axis([r*col-0.5, (r+1)*col-0.5, -5, 5])    #min and max of the x and y axes
-		
-		#consensus table
-		rnames=["consensus"]
-		celltext=[consensus[col*r:col*(r+1)]]
-		tabCons = plt.table(cellText=celltext, cellLoc='center', rowLabels = rnames, colWidths=widths, bbox=[0, 1.1, 1, 0.07])
-		tabCons.set_fontsize(8)
+		if r==0:
+			#consensus table
+			tabCons = plt.table(cellText=[consensus[col*r:col*(r+1)]], cellLoc='center', rowLabels = ["consensus"], colWidths=widths, bbox=[0, 1.1, 1, 0.07])
+			tabCons.auto_set_font_size(False)
+			tabCons.set_fontsize(8)
 
-		#structure table
-		rnames=["structure"]
-		celltext=[stru[col*r:col*(r+1)]]
-		tabStru = plt.table(cellText=celltext, cellLoc='center', rowLabels = rnames, colWidths=widths, bbox=[0, 1.02, 1, 0.07])
-		tabStru.set_fontsize(8)
+			#structure table
+			tabStru = plt.table(cellText=[stru[col*r:col*(r+1)]], cellLoc='center', rowLabels = ["structure"], colWidths=widths, bbox=[0, 1.02, 1, 0.07])
+			tabStru.auto_set_font_size(False)
+			tabStru.set_fontsize(8)
+		else:
+			#consensus table
+			tabCons = plt.table(cellText=[consensus[col*r:col*(r+1)]], cellLoc='center', colWidths=widths, bbox=[0, 1.1, 1, 0.07])
+			tabCons.set_fontsize(8)
 
-	#TODO: w oknie wyswietla sie dobrze, ale w zapisanym obrazku tabele sa rozciagniate i przesuniete + cos sie psuje z rozmiarem czcionki
-	if float(col)/(l-col*rows)>2:
-		cols=np.ceil(float(col)/(l-col*rows))
-	else:
-		cols=1
-	
-	tmp=fig.add_subplot(rows+2, cols, (rows+1)*cols+1, xlabel="Amino Acid", ylabel='Hydrophobicity')
-	for i in xrange(l-col*rows):
-		#print "r, i", rows, i
-		c = (1, (chain[col*rows+i]-minC)/(maxC-minC), 0)    #bar color
-		tmp.bar(col*rows+i, hydro[col*rows+i], width, color=c, align='center', linewidth=1)
-	plt.axis([rows*col-0.5, l-0.5, -5, 5])    #min and max of the x and y axes
-	plt.xticks(range(col*rows,l, 5))
+			#structure table
+			tabStru = plt.table(cellText=[stru[col*r:col*(r+1)]], cellLoc='center', colWidths=widths, bbox=[0, 1.02, 1, 0.07])
+			tabStru.set_fontsize(8)
+
+	#last (shorter) barchart
+	tmp=plt.axes([margin, margin, (1-2*margin)*(l-col*(rows-2))/float(col), height], xlabel="Amino Acid", ylabel='Hydrophobicity')
+	for i in xrange(l-col*(rows-2)):
+		c = (1, (chain[col*(rows-2)+i]-minC)/(maxC-minC), 0)    #bar color
+		tmp.bar(col*(rows-2)+i, hydro[col*(rows-2)+i], width, color=c, align='center', linewidth=1)
+	plt.axis([(rows-2)*col-0.5, l-0.5, -5, 5])    #min and max of the x and y axes
+	plt.xticks(range(col*(rows-2),l, 5))
     	
-	rnames=["consensus"]
-	celltext=[consensus[col*rows:]]
-	widths=[1.0/col]*(l-col*rows)
-	tabCons = plt.table(cellText=celltext, cellLoc='center', rowLabels = rnames, colWidths=widths, bbox=[0, 1.1, 1, 0.07])
-	#tabCons.set_fontsize(8)
+	widths=[1.0/col]*(l-col*(rows-2))
+	tabCons = plt.table(cellText=[consensus[col*(rows-2):]], cellLoc='center', colWidths=widths, bbox=[0, 1.1, 1, 0.07])
+	tabCons.auto_set_font_size(False)
+	tabCons.set_fontsize(8)
 
-	rnames=["structure"]
-	celltext=[stru[col*rows:]]
-	tabStru = plt.table(cellText=celltext, cellLoc='center', rowLabels = rnames, colWidths=widths, bbox=[0, 1.02, 1, 0.07])
-	#tabStru.set_fontsize(8)
+	tabStru = plt.table(cellText=[stru[col*(rows-2):]], cellLoc='center', colWidths=widths, bbox=[0, 1.02, 1, 0.07])
+	tabStru.auto_set_font_size(False)
+	tabStru.set_fontsize(8)
+	
 	plt.savefig('test')
 	return fig
 
-def okno(fig):
+def window(fig):
 	win = gtk.Window()
 	win.connect("destroy", gtk.main_quit)
-	win.set_default_size(800, 1200)
+	win.set_default_size(1200, 1200)
     	win.set_title("MSA Visualisation")
         
     	canvas = FigureCanvas(fig)
