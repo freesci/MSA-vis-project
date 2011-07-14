beec51831e971c763ce23037e51c44e08d2791a7
e1809 <marta@marta-P5K.(none)>
2011-07-15 01:22:27 +0200

    chart 3.0

--- chart.py
+++ chart.py
@@ -1,5 +1,4 @@
 import gtk
-import random
 import matplotlib
 matplotlib.use('Svg')
 import matplotlib.cm as cm
@@ -54,78 +53,108 @@ arguments:
 	hydro - average hydrophobicity (list) 
 	chain - average side chain volume (list)
 	stru - secondary structure (list)
+	cd_hit - cd-hit output (sequence dctionary)
 	col - number of columns (default=30)
 """
 
-def chart(consensus, hydro, chain, stru, col=50):
+def chart(consensus, hydro, chain, stru, cd_hit, filename, col):
 	minC=60.0	#min side-chain volume
 	maxC=230.0	#max side-chain volume
 	l = len(consensus)
-	if col==0: col=l
-	rows = int(np.ceil(float(l)/col))+1	#number of rows
-	margin = 0.1
-	height=(1-margin*(rows+1))/rows		#barchart height
+	k = len(cd_hit)
+	rows = int(np.ceil(float(l)/col))	#number of rows
 
-	fig = plt.figure(figsize =(col/3,rows*8))
+	fig = plt.figure(figsize =(col/3,rows*(9+k/2.0)+4))
 
-	wykr=plt.axes([margin, 1-(margin+height), 0.8, height])
-	wykr.set_title("MSA Visualisation", y=1)
+	inchH=1.0/(rows*(9+k/2.0)+4)
+	colBarH = 3.0*inchH	#colorbar height
+	margin = 2.0*inchH
+	height=6.0*inchH		#barchart height
+	seqH = (k/2.0+1.0)*inchH	#sequence table height
+
+	inchW = 1.0/(col/3)
+
+	wykr=plt.axes([2*inchW, 1-colBarH, 1-4*inchW, colBarH])
+	wykr.set_title("MSA Visualisation", y=0.4)
 	plt.axis('off')
 
 	#side chain volume colorbar
 	m=cm.ScalarMappable(cmap=cm.autumn)
 	m.set_array(np.array([minC, maxC]))
-	cbr=plt.colorbar(m, orientation='horizontal', fraction=0.7)
+	cbr=plt.colorbar(m, orientation='horizontal', fraction=0.4)
 	cbr.set_label('Side Chain Volume')
 
 
 	width = 1
 	widths=[1.0/col]*col
-	for r in xrange(rows-2):
-
+	for r in xrange(rows):
 		#barchart
-		tmp=plt.axes([margin, 1-(r+2)*(margin+height), 1-2*margin, height], xlabel="Amino Acid", ylabel='Hydrophobicity')
+		if  r==rows-1:
+			tmp=plt.axes([inchW, 1-(r+1)*(margin+height+seqH)-colBarH+seqH, (1-2*inchW)*(l-col*(rows-1))/float(col), height], xlabel="Amino Acid", ylabel='Hydrophobicity')
+			plt.axis([(rows-1)*col-0.5, l-0.5, -5, 5])    #min and max of the x and y axes
+			plt.xticks(range(col*(rows-1),l, 5))
+		else:
+			tmp=plt.axes([inchW, 1-(r+1)*(margin+height+seqH)-colBarH+seqH, 1-2*inchW, height], xlabel="Amino Acid", ylabel='Hydrophobicity')
+		    	plt.axis([r*col-0.5, (r+1)*col-0.5, -5, 5])    #min and max of the x and y axes
+			plt.xticks(range(col*r,(r+1)*col, 5))
+
 		for i in xrange(col):
+			if r==rows-1 and i==l-col*(rows-1): break	#break if last chart is shorter
 			c = (1, (chain[col*r+i]-minC)/(maxC-minC), 0)    #bar color
 			tmp.bar(col*r+i, hydro[col*r+i], width, color=c, align='center', linewidth=1)
-	    	plt.axis([r*col-0.5, (r+1)*col-0.5, -5, 5])    #min and max of the x and y axes
+
 		if r==0:
 			#consensus table
 			tabCons = plt.table(cellText=[consensus[col*r:col*(r+1)]], cellLoc='center', rowLabels = ["consensus"], colWidths=widths, bbox=[0, 1.1, 1, 0.07])
-			tabCons.auto_set_font_size(False)
-			tabCons.set_fontsize(8)
 
 			#structure table
 			tabStru = plt.table(cellText=[stru[col*r:col*(r+1)]], cellLoc='center', rowLabels = ["structure"], colWidths=widths, bbox=[0, 1.02, 1, 0.07])
-			tabStru.auto_set_font_size(False)
-			tabStru.set_fontsize(8)
+
+			#sequence table
+			text=[]
+			labels=[]
+			for key in sorted(cd_hit.keys()):
+				text.append(cd_hit[key][col*r:col*(r+1)])
+				labels.append(key+": ")
+			tabCdHit =  plt.table(cellText=text, cellLoc='center', colWidths=widths, rowLabels = labels, bbox=[0, -(k/2.0+1.0)/6.0, 1, k/12.0])
+
+		elif r==rows-1:
+			widths=[1.0/col]*(l-col*(rows-1))
+
+			#consensus table
+			tabCons = plt.table(cellText=[consensus[col*(rows-1):]], cellLoc='center', colWidths=widths, bbox=[0, 1.1, 1, 0.07])
+
+			#structure table
+			tabStru = plt.table(cellText=[stru[col*(rows-1):]], cellLoc='center', colWidths=widths, bbox=[0, 1.02, 1, 0.07])
+
+			#sequence table			
+			text=[]
+			for key in sorted(cd_hit.keys()):
+				text.append(cd_hit[key][col*r:col*(r+1)])
+			tabCdHit =  plt.table(cellText=text, cellLoc='center', colWidths=widths, bbox=[0, -(k/2.0+1.0)/6.0, 1, k/12.0])
 		else:
 			#consensus table
 			tabCons = plt.table(cellText=[consensus[col*r:col*(r+1)]], cellLoc='center', colWidths=widths, bbox=[0, 1.1, 1, 0.07])
-			tabCons.set_fontsize(8)
 
 			#structure table
 			tabStru = plt.table(cellText=[stru[col*r:col*(r+1)]], cellLoc='center', colWidths=widths, bbox=[0, 1.02, 1, 0.07])
-			tabStru.set_fontsize(8)
 
-	#last (shorter) barchart
-	tmp=plt.axes([margin, margin, (1-2*margin)*(l-col*(rows-2))/float(col), height], xlabel="Amino Acid", ylabel='Hydrophobicity')
-	for i in xrange(l-col*(rows-2)):
-		c = (1, (chain[col*(rows-2)+i]-minC)/(maxC-minC), 0)    #bar color
-		tmp.bar(col*(rows-2)+i, hydro[col*(rows-2)+i], width, color=c, align='center', linewidth=1)
-	plt.axis([(rows-2)*col-0.5, l-0.5, -5, 5])    #min and max of the x and y axes
-	plt.xticks(range(col*(rows-2),l, 5))
-    	
-	widths=[1.0/col]*(l-col*(rows-2))
-	tabCons = plt.table(cellText=[consensus[col*(rows-2):]], cellLoc='center', colWidths=widths, bbox=[0, 1.1, 1, 0.07])
-	tabCons.auto_set_font_size(False)
-	tabCons.set_fontsize(8)
+			#sequence table			
+			text=[]
+			for key in sorted(cd_hit.keys()):
+				text.append(cd_hit[key][col*r:col*(r+1)])
+			tabCdHit =  plt.table(cellText=text, cellLoc='center', colWidths=widths, bbox=[0, -(k/2.0+1.0)/6.0, 1, k/12.0])
+		
+		tabCons.auto_set_font_size(False)
+		tabCons.set_fontsize(8)
+		tabStru.auto_set_font_size(False)
+		tabStru.set_fontsize(8)
+		tabCdHit.auto_set_font_size(False)
+		tabCdHit.set_fontsize(12)
+		for v in tabCdHit.get_celld().values():
+			v.set_edgecolor('w')
 
-	tabStru = plt.table(cellText=[stru[col*(rows-2):]], cellLoc='center', colWidths=widths, bbox=[0, 1.02, 1, 0.07])
-	tabStru.auto_set_font_size(False)
-	tabStru.set_fontsize(8)
-	
-	plt.savefig('test')
+	plt.savefig(filename)
 	return fig
 
 def window(fig):
