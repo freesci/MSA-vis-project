c89420e3c469b34dceef5fe4a455ab510b418b5e
nashirav <nashirav@gmail.com>
2011-08-15 11:58:36 +0300

    Edited msa_vis/msa_vis_app/models.py via GitHub

--- msa_vis/msa_vis_app/models.py
+++ msa_vis/msa_vis_app/models.py
@@ -7,8 +7,8 @@ from django.db import models
 
 
 class Page(models.Model):
-  #sequences = models.TextField(max_length = "150",help_text="(Max dlugosc seq <= 150)") # help_text nie jest najladniejszym rozwiazaniem w widoku formularzu...
-  #upload_file = models.FileField(upload_to = "uploaded_files", help_text="(Wybierz plik w formacie fasta)") #storage=fs # uploaded_files jest katalogiem tworzanym wzgledem MEDIA_ROOT
+  sequences = models.TextField(max_length = "150",help_text="(Max dlugosc seq <= 150)") # help_text nie jest najladniejszym rozwiazaniem w widoku formularzu...
+  upload_file = models.FileField(upload_to = "uploaded_files", help_text="(Wybierz plik w formacie fasta)") #storage=fs # uploaded_files jest katalogiem tworzanym wzgledem MEDIA_ROOT
   email = models.EmailField(blank=True, max_length = "20",help_text="(narazie jest obowiazkowe)") #blank=True, aby to pole moglo pozostac puste przy wypelnianiu pol
   #seqID = models.CharField(max_length = "40") # podobe pole do tego ze strony aln2plot - tam jesli uzytkownik nic w to pole nie wipisze, program sam wygeneruje losowe id
   
@@ -23,7 +23,7 @@ from django.forms import ModelForm
 class PageForm(ModelForm):
   class Meta:
     model = Page # w klasie meta mus byc pole o nazwie model, do jakiego modelu ma byc stworzony formularz
-    #fields = ('sequences', 'upload_file','email') # w tej kolejnosci pokaza sie te rzeczy w formularzu na stronie
+    fields = ('sequences', 'upload_file','email') # w tej kolejnosci pokaza sie te rzeczy w formularzu na stronie
     
     
     
