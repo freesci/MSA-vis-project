064f9b5eb5f015ef02e64d44fd3e066e28e21fde
nashirav <nashirav@gmail.com>
2011-08-18 11:17:15 +0300

    Edited msa_vis/msa_vis_app/views.py via GitHub

--- msa_vis/msa_vis_app/views.py
+++ msa_vis/msa_vis_app/views.py
@@ -14,12 +14,10 @@ from models import PageForm
 def first_page(request):
     if request.method == 'POST': # If the form has been submitted... # wysylanie wypelnione dane z formularza, a gdy 'GET" to to w innych sytuacjach, gdy np oglada uzytkownik strone, patrz protokol HTTP
       form = PageForm(request.POST) # A form bound to the a data
-      print("/n",form.as_table)
       if form.is_valid(): # All validation rules pass
 	email = form.cleaned_data["email"]
 	sequences = form.cleaned_data["sequences"]
 	seqID = form.cleaned_data["seqID"]
-	#print(dir(form))
 	form.save()
         return render_to_response("second_page.html",{"email":email,"seqID":seqID})
       else: # napisac wyjatek jesli uztkownik zle wypelni jakies pole
